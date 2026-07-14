2c67316b56483f581b5e62ce5bae89d6e9b54ce5
leonid-topal <leonvil093@gmail.com>
2026-07-14 19:40:18 +0200

    test commit - 4

--- src/main/java/com/example/demogit/demogit/UserService.java
+++ src/main/java/com/example/demogit/demogit/UserService.java
@@ -3,5 +3,7 @@ package com.example.demogit.demogit;
 public class UserService {
     public void getUser(){
         System.out.println("523");
+        System.out.println("523");
+        System.out.println("523");
     }
 }
