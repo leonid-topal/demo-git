503cbcea7362d3f70698626a9dafc44dff3e69bd
leonid-topal <leonvil093@gmail.com>
2026-07-14 19:55:35 +0200

    Revert "test commit - 4"
    This reverts commit 2c67316b56483f581b5e62ce5bae89d6e9b54ce5.

--- src/main/java/com/example/demogit/demogit/UserService.java
+++ src/main/java/com/example/demogit/demogit/UserService.java
@@ -3,7 +3,5 @@ package com.example.demogit.demogit;
 public class UserService {
     public void getUser(){
         System.out.println("523");
-        System.out.println("523");
-        System.out.println("523");
     }
 }
